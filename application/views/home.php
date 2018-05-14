49d58fa97dbc2339031f9413f3bad229b4c5df29
Mateus <mateus-vinicius19@live.com>
2018-05-14 16:39:52 -0300

    primeiro envio

---
+++ application/views/home.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Plataforma De Bioinformática </title>
+	
+    <!-- css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/nivo-lightbox.css" rel="stylesheet" />
+	<link href="assets/css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/animate.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet">
+
+	<!-- template skin -->
+	<link id="t-colors" href="assets/color/default.css" rel="stylesheet">
+
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+
+<div id="wrapper">
+	
+    <nav class="navbar navbar-custom" role="navigation">
+        <div class="container navigation">
+		
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+			  <ul class="nav navbar-nav">
+				<li><div id="cart"></div></li>
+				<li class="active"><a href="#intro">Home</a></li>
+				<li><a href="#">Papers <span class="label label-danger"></span></a> </li>
+
+			  </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+
+        </div>
+        <!-- /.container -->
+    </nav>
+	
+	<!-- Section: intro -->
+    <section id="intro" class="intro">
+		<div class="intro-content">
+			<div class="container">
+				
+				<div class="row">
+					<div class="col-sm-6 col-md-6 col-lg-6">
+					<div class="wow fadeInDown" data-wow-offset="0" data-wow-delay="0.1s">
+						<img src="assets/img/plataforma.png" class="img-responsive" alt="" />
+
+					</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-6 slogan">
+						<div class="wow fadeInUp" data-wow-duration="2s" data-wow-delay="0.2s">
+						<h2>Sobre Plataforma</h2>
+						<p>
+						Lorem ipsum dolor sit amet, tota senserit percipitur ius ut, usu et fastidii forensibus voluptatibus. His ei nihil feugait. Illum ludus abhorreant te vim
+						
+						</p>
+						<p>
+						Dicta fierent sea ne, no duo meis rebum facer, pri odio scripta tincidunt id. Per admodum inimicus iracundia ad
+						</p>
+						</div>
+						<div class="buttons">
+						</div>				
+					</div>					
+				</div>		
+			</div>
+		</div>		
+    </section>
+	
+	<!-- /Section: intro -->
+	<div class="divider-short"></div>
+    <section id="content1" class="home-section">
+		<div class="container">
+			<div class="row text-center heading">
+				<h3>Equipe</h3>
+			</div>
+			<div class="row">
+			<center>	
+				<div class="col-sm-4">
+					<div class="wow fadeInRight" data-wow-delay="0.3s">
+						<div class="features">	
+						<a href="http://lattes.cnpq.br/9908602398727888">						
+							<img src="assets/img/ricardo.png" alt="Ricardo" class="img-circle"> 
+							<h5>Ricardo Khouri</h5>
+						</a> 
+							
+						</div>
+				</div>
+				</div>
+				<div class="col-sm-4">
+				  <div class="wow fadeInRight" data-wow-delay="0.3s">	
+					<div class="features">	
+					<a href="http://lattes.cnpq.br/7076978352521394">							
+						<img src="assets/img/felipe.png" alt="Felipe" class="img-circle">
+								<h5>Felipe Torres</h5>
+					</a>
+                    </div>			
+					</div>
+				</div>
+				<div class="col-sm-4" >
+				  <div class="wow fadeInRight" data-wow-delay="0.3s">	
+				    <div class="features">	
+					<a href="http://lattes.cnpq.br/2485299591819455">							
+						<img src="assets/img/mateus.png" alt="Mateus" class="img-circle">
+							<h5>Mateus Vinicius</h5>
+					</a>
+					</div>
+				  </div>
+				</div>			
+					</div>
+				</div>
+			</center>
+		</div>
+
+	</section>
+	<!-- /Section: content -->
+	
+	<div class="divider-short"></div>
+	
+    <section id="content2" class="home-section">
+	
+		<div class="container">
+			<div class="row text-center heading">
+				<h3>Estrutura</h3>
+			</div>
+			
+			
+			<div class="row">
+				<div class="col-md-6">
+					<div class="wow fadeInLeft" data-wow-delay="0.2s">
+						<p>
+						Lorem ipsum dolor sit amet, ipsum vocent reprimique ad eos, ludus option signiferumque an pro. Id case quaestio mei, an ipsum offendit vel, ex ius quis comprehensam. Eu per illud modus fabellas, eam an brute gubergren repudiandae.
+						</p>
+						<p>
+						Eam voluptua assentior ea, an eirmod sensibus scribentur vel. Elit nonumy indoctum per eu. Feugait omittantur ne qui, commodo invidunt ea nam
+						</p>
+						<div class="divider-short marginbot-30 margintop-30"></div>
+						<div class="features">							
+							<a href="http://www.clcbio.com/files/usermanuals/CLC_Main_Workbench_User_Manual.pdf">
+								<h5><span class="glyphicon glyphicon-hdd"></span>&nbsp CLC Main Workbench 6.9.</h5>
+							</a>
+						</div>
+						<div class="features">	
+							<a href="http://tools.lifetechnologies.com/content/sfs/manuals/VectorNTIExpressUG.pdf">					
+								<h5><span class="glyphicon glyphicon-hdd"></span>&nbsp  Vector NTI Express.</h5>
+							</a>	
+						</div>
+						<div class="features">
+							<a href="https://www.dnastar.com/t-support-tutorials.aspx">						
+								<h5><span class="glyphicon glyphicon-hdd"></span>&nbsp DNAStar Lasergene Core Suite 11, Array Star 11, SeqMan NGen 11.</h5>
+							</a>		
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="wow fadeInRight" data-wow-delay="0.3s">
+						<img src="img/img-2.png" alt="" class="img-responsive" />
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+	</section>
+	<!-- /Section: content -->
+	
+	<div class="divider-short"></div>
+    <section id="works" class="home-section text-center">
+		<div class="container">
+			<div class="row text-center heading">
+				<h3>Papers</h3>
+			</div>
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12" >
+
+                    <div class="row gallery-item">
+                        <div class="col-md-3">
+							<a href="#" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+								<img src="#" class="img-responsive" alt="img">
+							</a>
+						</div>
+						<div class="col-md-3">
+							<a href="#" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+								<img src="#" class="img-responsive" alt="img">
+							</a>
+						</div>
+						<div class="col-md-3">
+							<a href="#" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+								<img src="#" class="img-responsive" alt="img">
+							</a>
+						</div>
+						<div class="col-md-3">
+							<a href="#" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+								<img src="#" class="img-responsive" alt="img">
+							</a>
+						</div>
+					</div>
+	
+                </div>
+            </div>	
+		</div>
+	</section>
+	<footer>
+		<div class="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-md-6 col-lg-6">
+					
+				
+					<div class="wow fadeInLeft" data-wow-delay="0.1s">
+					<div class="text-left">
+					<p>&copy;Created By Mateus Vinicius.</p>
+					</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-6 col-lg-6">
+					<div class="wow fadeInRight" data-wow-delay="0.1s">
+					<div class="text-right margintop-10">
+						<img src="assets/img/fiocruz.png" alt="">
+					</div>
+					</div>
+				</div>
+			</div>	
+		</div>
+		</div>
+	</footer>
+
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+
+	<!-- Core JavaScript Files -->
+    <script src="assets/js/jquery.min.js"></script>	 
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/jquery.scrollTo.js"></script>
+	<script src="assets/js/nivo-lightbox.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+
+</body>
+
+</html>
